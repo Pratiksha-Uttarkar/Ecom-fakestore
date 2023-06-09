93d0745693b821ab61f21c88ad1e78a9d2142afb
Pratiksha-Uttarkar <131133934+Pratiksha-Uttarkar@users.noreply.github.com>
2023-06-09 20:03:40 +0530

    added carousel in home page (#1)

--- src/components/PreviewPage.js
+++ src/components/PreviewPage.js
@@ -14,28 +14,91 @@ const Previewpage = () => {
   return (
     <>
       {checkoutData !== null ? (
-        <div>
-          <h2 style={{ textAlign: "center",fontSize: 60 }}>Preview Page</h2>
+        <div className="container w-50 p-2">
+          <h2 style={{ textAlign: "center", fontSize: 60 }}>Preview Page</h2>
+          <div>
+          <table class="table table-bordered">
+            <thead class="thead-light">
+              <tr>
+                <th
+                  scope="col"
+                  style={{
+                    padding: "15px",
+                    marginLeft: "100px",
+                    paddingLeft:"2%",
+                    fontSize: 30,
+                  }}
+                >
+                  Billing Address
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">
+                  <p
+                    style={{
+                      paddingTop: "25px",
+                      marginLeft: "100px",
+                      fontSize: 20,
+                    }}
+                  >
+                    First Name: {checkoutData.fname}
+                  </p>
+                </th>
+              </tr>
+            </tbody>
 
-          <h3 style={{ paddingTop: "25px", marginLeft: "100px",fontSize: 30}}>
-            Billing Address
-          </h3>
+            <tbody>
+              <tr>
+                <th scope="row">
+                  <p
+                    style={{
+                      paddingTop: "25px",
+                      marginLeft: "100px",
+                      fontSize: 20,
+                    }}
+                  >
+                    Last Name: {checkoutData.lname}
+                  </p>
+                </th>
+              </tr>
+            </tbody>
 
-          <p style={{ paddingTop: "25px", marginLeft: "100px", fontSize: 20 }}>
-            First Name: {checkoutData.fname}
-          </p>
+            <tbody>
+              <tr>
+                <th scope="row">
+                  <p
+                    style={{
+                      paddingTop: "25px",
+                      marginLeft: "100px",
+                      fontSize: 20,
+                    }}
+                  >
+                    Email: {checkoutData.email}
+                  </p>
+                </th>
+              </tr>
+            </tbody>
 
-          <p style={{ paddingTop: "25px", marginLeft: "100px", fontSize: 20 }}>
-            Last Name: {checkoutData.lname}
-          </p>
+            <tbody>
+              <tr>
+                <th scope="row">
+            <p
+              style={{
+                paddingTop: "25px",
+                marginLeft: "100px",
+                fontSize: 20,
+              }}
+            >
+              Address: {checkoutData.address}
+            </p>
+            </th>
+            </tr>
+            </tbody>
 
-          <p style={{ paddingTop: "25px", marginLeft: "100px", fontSize: 20 }}>
-            Email: {checkoutData.email}
-          </p>
-
-          <p style={{ paddingTop: "25px", marginLeft: "100px", fontSize: 20 }}>
-            Address: {checkoutData.address}
-          </p>
+          </table>
+          </div>
         </div>
       ) : (
         <></>
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { addCart } from "../redux/action";
 import { useEffect } from "react";
 
@@ -14,21 +14,31 @@ export default function ProductItem({ product }) {
     <>
       <div className="col-md-3 mb-4">
         <div class="card h-100 text-center p4" key={product.id}>
+          <Link to={`/products/${product.id}`}>
           <img
             src={product.image}
             class="card-img-top"
             alt={product.title}
             height="250px"
-            style={{ objectFit: "contain" }}
+            style={{ objectFit: "contain", padding: "16px" }}
           />
+          </Link>
           <div class="card-body">
             <h5 class="card-title mb-0">{product.title.substring(0, 12)}...</h5>
             <p class="card-text lead fw-bold">${product.price}</p>
 
             <NavLink
-              className="btn btn-outline-dark"
-              style={{ width: "200px", height: "50px", marginBottom: "15px", borderRadius:"20px" }}
-              to={`/products/${product.id}`}
+              className="btn btn-outline-dark px-4 py-2"
+              style={{
+                width: "200px",
+                borderRadius: "20px",
+                height: "50px",
+                paddingBottom: "10px",
+                textAlign: "center",
+                marginBottom: "15px",
+              }}
+              to="/cart"
+              onClick={() => addProduct(product)}
               class="btn btn-outline-dark"
             >
               Buy Now
@@ -38,9 +48,10 @@ export default function ProductItem({ product }) {
               onClick={() => addProduct(product)}
               style={{
                 width: "200px",
-                borderRadius:"20px",
+                borderRadius: "20px",
                 height: "50px",
                 paddingBottom: "10px",
+                textAlign: "center",
               }}
             >
               Add to cart
